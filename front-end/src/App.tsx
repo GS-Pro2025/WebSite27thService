8d18f7a8e67daa59fb0c63cc8e713742bf0e55b3
J2517 <jackeline.rivera23296@ucaldas.edu.co>
2025-07-11 08:05:21 -0500

    Propuesta de diseño Home

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,14 @@
 import Navbar from './components/navbar'
 import './App.css'
 import Home from './pages/home'
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="w-full overflow-visible">
       <Navbar />
       <Home/>
+            <Footer />
     </div>
   )
 }
--- front-end/src/pages/home.tsx
+++ front-end/src/pages/home.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import HeroSection from '../components/heroSection';
 import ProcessSection from '../components/processSection';
+import UsSection  from '../components/UsSection';
+import ServicesSection from '../components/ServicesSection';  
 
 const Home: React.FC = () => {
   return (
     <div className="w-full min-h-screen bg-white overflow-visible">
       <HeroSection />
       <ProcessSection />
+      <UsSection />
+      <ServicesSection />
     </div>
   );
 };
