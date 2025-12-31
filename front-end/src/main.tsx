5a56a2463e75c2bb92d22dd51dbbddd190f116ef
GS-Pro2025 <Manager-Support@gspromastermoving.com>
2025-12-31 09:16:02 -0500

    Update main.tsx

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,19 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 import { LoadScript } from '@react-google-maps/api'
 import './index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <LoadScript
-        googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
-        libraries={['places', 'geometry']}
-      >
-        <App />
-      </LoadScript>
-    </GoogleOAuthProvider>
+    <LoadScript
+      googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
+      libraries={['places', 'geometry']}
+    >
+      <App />
+    </LoadScript>
   </StrictMode>,
 )
